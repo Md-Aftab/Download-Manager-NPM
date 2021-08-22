8869356c0b32ef7ae9111f7a5b85ed7f2ca1d93d
Md-Aftab <aftabalam25199@gmail.com>
2021-08-23 02:05:26 +0530

    first commit

---
+++ android/src/main/java/com/asterinet/react/bgactions/BackgroundTaskOptions.java
@@ -0,0 +1,135 @@
+package com.asterinet.react.bgactions;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import android.os.Parcelable;
+import android.util.Log;
+import android.widget.Button;
+
+import androidx.annotation.ColorInt;
+import androidx.annotation.IdRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public final class BackgroundTaskOptions {
+    private final Bundle extras;
+
+    public BackgroundTaskOptions(@NonNull final Bundle extras) {
+        this.extras = extras;
+    }
+
+    public BackgroundTaskOptions(@NonNull final ReactContext reactContext, @NonNull final ReadableMap options) {
+        // Create extras
+        extras = Arguments.toBundle(options);
+        if (extras == null)
+            throw new IllegalArgumentException("Could not convert arguments to bundle");
+        // Get taskTitle
+        try {
+            if (options.getString("taskTitle") == null)
+                throw new IllegalArgumentException();
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Task title cannot be null");
+        }
+        // Get taskDesc
+        try {
+            if (options.getString("taskDesc") == null)
+                throw new IllegalArgumentException();
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Task description cannot be null");
+        }
+        // Get iconInt
+        try {
+            final ReadableMap iconMap = options.getMap("taskIcon");
+            if (iconMap == null)
+                throw new IllegalArgumentException();
+            final String iconName = iconMap.getString("name");
+            final String iconType = iconMap.getString("type");
+            String iconPackage;
+            try {
+                iconPackage = iconMap.getString("package");
+                if (iconPackage == null)
+                    throw new IllegalArgumentException();
+            } catch (Exception e) {
+                // Get the current package as default
+                iconPackage = reactContext.getPackageName();
+            }
+            final int iconInt = reactContext.getResources().getIdentifier(iconName, iconType, iconPackage);
+            extras.putInt("iconInt", iconInt);
+            if (iconInt == 0)
+                throw new IllegalArgumentException();
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Task icon not found");
+        }
+
+ // Get actions
+        try {
+            final ReadableArray acts = options.getArray("actions");
+            Bundle actions = new Bundle();
+            if(acts != null) {
+                 for (int i = 0; i < acts.size(); i++) {
+                    ReadableMap map = acts.getMap(i);
+                    Bundle action = new Bundle();
+                    action.putString("title", map.getString("title"));
+                    action.putString("URI", map.getString("URI"));
+                    actions.putBundle(Integer.toString(i), action);
+                }
+            }
+            extras.putBundle("actions", actions);
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+
+        // Get color
+        try {
+            final String color = options.getString("color");
+            extras.putInt("color", Color.parseColor(color));
+        } catch (Exception e) {
+            extras.putInt("color", Color.parseColor("#ffffff"));
+        }
+    }
+
+    public Bundle getExtras() {
+        return extras;
+    }
+
+    public String getTaskTitle() {
+        return extras.getString("taskTitle", "");
+    }
+
+    public String getTaskDesc() {
+        return extras.getString("taskDesc", "");
+    }
+
+    @IdRes
+    public int getIconInt() {
+        return extras.getInt("iconInt");
+    }
+
+    @ColorInt
+    public int getColor() {
+        return extras.getInt("color");
+    }
+
+    @Nullable
+    public String getLinkingURI() {
+        return extras.getString("linkingURI");
+    }
+     @Nullable
+    public Bundle getActions() {
+        return extras.getBundle("actions");
+    }
+
+    @Nullable
+    public Bundle getProgressBar() {
+        return extras.getBundle("progressBar");
+    }
+}
